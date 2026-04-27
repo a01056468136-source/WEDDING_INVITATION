e754f87578d73e882d10e2892f2d8172bf301fc0
a01056468136-source <a01056468136@gmail.com>
2026-04-27 22:11:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const WEDDING_INVITATION_URL = "https://a01056468136-source.github.io/WEDDING_INVITATION/";
 export const KAKAOTALK_API_TOKEN = "0a6ece6f6c13c28546b3020024f9f55d";
 export const KAKAOTALK_SHARE_IMAGE =
-  "https://raw.githubusercontent.com/a01056468136-source/WEDDING_INVITATION/refs/heads/main/src/assets/Gallery_Photo_7.webp";
+  "https://a01056468136-source.github.io/WEDDING_INVITATION/thumbnail.jpg";
 
 export const WEDDING_DATE = "26년 12월 12일(토) 오후 2시";
 export const WEDDING_LOCATION = "공군호텔 웨딩홀 1층 컨벤션홀";
