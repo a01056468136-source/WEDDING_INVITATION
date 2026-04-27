2136419840a917d00a8efed274ea56f3d98cc3fc
a01056468136-source <a01056468136@gmail.com>
2026-04-27 17:50:06 +0900

    스크립트 추가

---
+++ gatsby-ssr.js
@@ -0,0 +1,19 @@
+const React = require("react");
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <meta property="og:type" content="website" key="og:type" />,
+    <meta property="og:title" content="동호❤가영 결혼식에 초대합니다" key="og:title" />,
+    <meta property="og:description" content="저희의 소중한 날에 초대합니다." key="og:description" />,
+    <meta
+      property="og:image"
+      content="https://a01056468136-source.github.io/WEDDING_INVITATION/thumbnail.jpg"
+      key="og:image"
+    />,
+    <meta
+      property="og:url"
+      content="https://a01056468136-source.github.io/WEDDING_INVITATION/"
+      key="og:url"
+    />,
+  ]);
+};
