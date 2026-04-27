29b1cc06f40896526bc387c429e6398c6e723991
a01056468136-source <a01056468136@gmail.com>
2026-04-27 17:51:22 +0900

    섬네일추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
    pathPrefix: "/WEDDING_INVITATION",
   siteMetadata: {   
-    title: `weddingInvitation`,
-    siteUrl: `https://a01056468136-source.github.io/WEDDING_INVITATION`
+    title: `동호❤가영 결혼식에 초대합니다`,
+    siteUrl: `https://a01056468136-source.github.io/WEDDING_INVITATION`,
+    image: "/thumbnail.jpg"
   },
   plugins: ["gatsby-plugin-styled-components"]
 };
