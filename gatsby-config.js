ae20002e69441467f2cc6c48b1b9b00a706862a4
a01056468136-source <a01056468136@gmail.com>
2026-04-27 05:58:24 +0000

    Updates

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    pathPrefix: "/WEDDING_INVITATION",
-    title: `weddingInvitation`,
-    siteUrl: `https://a01056468136-source.github.io/WEDDING_INVITATION`
-  },
-  plugins: ["gatsby-plugin-styled-components"]
-};
