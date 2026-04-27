705e593929fa98e60660123e73e3a1cf239088f7
a01056468136-source <a01056468136@gmail.com>
2026-04-27 15:17:02 +0900

    사진수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  siteMetadata: {
-    pathPrefix: "/WEDDING_INVITATION",
+   pathPrefix: "/WEDDING_INVITATION",
+  siteMetadata: {   
     title: `weddingInvitation`,
     siteUrl: `https://a01056468136-source.github.io/WEDDING_INVITATION`
   },
